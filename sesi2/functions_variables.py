3b3c3c78b9cb46f8e724a893d87c44556ea49b72
naghi <yusufnaghi05@gmail.com>
2026-07-23 10:47:50 +0700

    initial commit

--- sesi2/functions_variables.py
+++
@@ -1,6 +0,0 @@
-# Ask user for their name
-name = input("what is your name?: ")
-
-# Say hello to user
-print("Hello,", name )
-
---
+++ sesi2/nol.py
@@ -0,0 +1,10 @@
+
+# Ask user for their name
+name = input("what is your name?: ")
+
+# Say hello to user
+print("Hello, ", end="???")
+print(name)
+
+print("halo", "coder", sep=", ", end="!\n")
+print("naghi")
