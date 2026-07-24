1400efd7f8393fe52a36663a8c0a5073683cec2d
naghi <yusufnaghi05@gmail.com>
2026-07-24 11:10:41 +0700

    initial commit

--- sesi2/nol/ask_user.py
+++ sesi2/nol/ask_user.py
@@ -1,5 +1,12 @@
 # Ask user for their name 
-name = input("What is your name?: ").strip().title()
+# name = input("What is your name?: ").strip().title()
 
 # Say hello to user 
-print(f"Hello, {name}")
+# print(f"Hello, {name}")
+
+def hello():
+    print(f"Hello, {name}")
+    
+name = input("What is your name?: ")
+
+print(hello)
--- sesi2/nol/calculator.py
+++ sesi2/nol/calculator.py
@@ -1,6 +1,6 @@
-x = 1
-y = 2
+x = float(input("What is x?"))
+y = float(input("What is y?"))
 
-z = x + y 
+z = (x / y)
 
-print(z)
+print(f"{z:.2f}")
