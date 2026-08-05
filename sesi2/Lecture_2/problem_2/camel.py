e38ddfbfa09b45dc87b0396630f5b6f4b7f7f6ee
naghi <yusufnaghi05@gmail.com>
2026-08-05 23:29:23 +0700

    initial commit

---
+++ sesi2/Lecture_2/problem_2/camel.py
@@ -0,0 +1,10 @@
+camel = input("camelCase: ")
+
+print("snake_case: ", end="")
+
+for x in camel:
+    if x.isupper():
+        print("_" + x.lower(), end="")
+    else:
+        print(x, end="")
+print()
