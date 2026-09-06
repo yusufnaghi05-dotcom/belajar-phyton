f9da8f372252c47f79e72512a25756e6e3d1930f
naghi <yusufnaghi05@gmail.com>
2026-09-06 17:03:23 +0700

    initial commit

---
+++ sesi2/Lecture_3/problem_3/grocery.py
@@ -0,0 +1,15 @@
+def main():
+    grocery()
+    
+    
+def grocery():
+    while True:
+        try:
+            item = input().upper()
+            for x in item:
+                if x == item:
+                        
+        except EOFError:
+            return item
+            
+main()
--- sesi2/Lecture_3/problem_3/taqueria.py
+++ sesi2/Lecture_3/problem_3/taqueria.py
@@ -1,29 +1,27 @@
-import sys
 def main():
-    item = get_price()
-    item1 = (f"{item:.2f}")
-    print(f"${item1}")
+    get_price()
 def get_price():
+    total = 0
+    food = {
+                    "Baja Taco": 4.25,
+                    "Burrito": 7.50,
+                    "Bowl": 8.50,
+                    "Nachos": 11.00,
+                    "Quesadilla": 8.50,
+                    "Super Burrito": 8.50,
+                    "Super Quesadilla": 9.50,
+                    "Taco": 3.00,
+                    "Tortilla Salad": 8.00
+                }
     while True:
         try:
             a = (input("Item: ")).title()
-            food = {
-                "Baja Taco": 4.25,
-                "Burrito": 7.50,
-                "Bowl": 8.50,
-                "Nachos": 11.00,
-                "Quesadilla": 8.50,
-                "Super Burrito": 8.50,
-                "Super Quesadilla": 9.50,
-                "Taco": 3.00,
-                "Tortilla Salad": 8.00
-            }
             if a in food:
-                ab = float((f"{food[a]}"))
-                return ab
-        except ValueError:
-            continue
+                ab = food[a] 
+                total = total + ab
+                print(f"${total:.2f}")
+                
         except EOFError:
-            sys.exit()
+            return total
 
 main()
