f78b6ba3f6fc0e9eaaf0734beb60bcddd64b0354
naghi <yusufnaghi05@gmail.com>
2026-09-07 13:16:41 +0700

    initial commit

--- sesi2/Lecture_3/problem_3/grocery.py
+++ sesi2/Lecture_3/problem_3/grocery.py
@@ -1,15 +1,16 @@
 def main():
     grocery()
-    
-    
 def grocery():
+    grocery = {}
     while True:
         try:
             item = input().upper()
-            for x in item:
-                if x == item:
-                        
+            if item in grocery:
+                grocery[item] = grocery[item] + 1
+            else:
+                grocery[item] = 1
         except EOFError:
-            return item
-            
+            break
+    for item in sorted(grocery):
+        print(grocery[item], item)
 main()
