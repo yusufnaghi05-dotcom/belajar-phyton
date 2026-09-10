0aaf326901ff3e280800c632411e413e39e226d1
naghi <yusufnaghi05@gmail.com>
2026-09-10 10:47:50 +0700

    initial commit

--- sesi2/Lecture_4/lesson_4/name.py
+++ sesi2/Lecture_4/lesson_4/name.py
@@ -1,3 +1,13 @@
-import sys
+# import sys
+# if len(sys.argv) < 2:
+#     print("too few arguments")
+# elif len(sys.argv) > 2:
+#     print("too many arguments")
+# else:
+#     print("hello my name is", sys.argv[1])
 
-print("hello, my name is", sys.argv[1])
+import sys
+if len(sys.argv) < 2:
+    print("too few arguments")
+for arg in sys.argv[1:-1]:
+    print("hello my name is", arg)
---
+++ sesi2/Lecture_4/lesson_4/say.py
@@ -0,0 +1,6 @@
+import cowsay
+import sys
+
+if len(sys.argv) == 2:
+    cowsay.cow("hello," + sys)
+ 
