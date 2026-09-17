96ebca0fd76b1effa18ad464b027ee8b04fcc5e3
naghi <yusufnaghi05@gmail.com>
2026-09-17 12:21:04 +0700

    initial commit

--- sesi2/Lecture_4/lesson_4/figlet.py
+++ sesi2/Lecture_4/lesson_4/figlet.py
@@ -2,12 +2,12 @@ import sys
 import pyfiglet
 import random
 if len(sys.argv) != 1 and len(sys.argv) != 3:
-    sys.exit()
+    sys.exit("Invalid usage")
 if len(sys.argv) == 3:
         if sys.argv[1] != "-f" and sys.argv[1] != "--font":
-            sys.exit()
+            sys.exit("Invalid usage")
         if sys.argv[2] not in pyfiglet.FigletFont.getFonts():
-            sys.exit()
+            sys.exit("Invalid usage")
 if len(sys.argv) == 1:
     the_font = random.choice(pyfiglet.FigletFont.getFonts())
 else:
